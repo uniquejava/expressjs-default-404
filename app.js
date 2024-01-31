c9a7850866efaddf84c2eff46a555f38a6fce3b1
uniquejava <uniquejava@gmail.com>
2024-01-31 22:34:34 +0800

    add CSP header

--- app.js
+++ app.js
@@ -17,6 +17,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(function(req, res, next) {
+  res.header("Content-Security-Policy", "script-src 'self' https://apis.google.com");
+  return next();
+});
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
